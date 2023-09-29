5165dcfbe40d2134384dc7e4ae9a131775843098
brunomrasilva <146493440+brunomrasilva@users.noreply.github.com>
2023-09-29 20:01:12 -0300

    Add files via upload

---
+++ All Game Komy.bat
@@ -0,0 +1,142 @@
+@echo off 
+CHCP 65001 >NUL
+::UTF-8
+color 0A 
+
+
+echo Instalando All Game Komy
+
+echo %time%
+
+echo Baixando arquivos de www.allgamekomy.com.br 
+echo.
+echo.
+
+echo baixando arquivos ..
+
+timeout 5 > NUL
+
+echo baixando arquivos 10%%.. (53/533MB)
+@echo off 
+
+@echo off
+
+timeout 8 > NUL
+
+echo baixando arquivos 30%%.. (159/533MB)
+@echo off 
+@echo off
+
+timeout  6 > NUL
+
+echo baixando arquivos 50%%.. (265/533MB)
+@echo off 
+@echo off
+
+timeout 5 > NUL
+
+echo baixando arquivos 75%%.. (397/533MB)
+@echo off 
+@echo off
+
+timeout 5 > NUL
+
+
+echo baixando arquivos 100%%.. (533/533MB)
+
+@echo off
+ 
+
+timeout 2 > NUL
+
+echo .........................................................................................................
+
+echo.
+echo descompactando arquivos 33%%..
+
+@echo off
+
+timeout 5 > NUL
+
+
+echo descompactando arquivos 66%%..
+
+@echo off
+
+timeout 5 > NUL
+
+echo descompactando arquivos 100%%..
+
+timeout 5 > NUL
+
+echo.
+echo.
+echo Instalando em -
+
+Echo "C:\Program Files (x86)\All Game Komy"
+
+@echo off
+
+timeout 15 > NUL
+
+echo Verificando instalação ..
+@echo off
+
+
+timeout 15
+
+echo.
+
+echo Integridade da Instalação 100%%
+
+@echo off
+
+timeout 3 > NUL
+
+echo.
+echo Verificando Conexão
+
+@echo off
+echo.
+ping www.allgame.com
+
+@echo off
+netstat -an | find "130.211.32.72"
+echo.
+echo.
+echo.
+echo Instalação concluida com êxito
+echo.
+echo.
+
+
+@echo off
+
+timeout 10 > NUL
+
+echo	⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⢉⢉⠉⠉⠻⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⣿⣿⣿⠟⠠⡰⣕⣗⣷⣧⣀⣅⠘⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⣿⣿⠃⣠⣳⣟⣿⣿⣷⣿⡿⣜⠄⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⡿⠁⠄⣳⢷⣿⣿⣿⣿⡿⣝⠖⠄⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⠃⠄⢢⡹⣿⢷⣯⢿⢷⡫⣗⠍⢰⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⡏⢀⢄⠤⣁⠋⠿⣗⣟⡯⡏⢎⠁⢸⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⠄⢔⢕⣯⣿⣿⡲⡤⡄⡤⠄⡀⢠⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⠇⠠⡳⣯⣿⣿⣾⢵⣫⢎⢎⠆⢀⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⠄⢨⣫⣿⣿⡿⣿⣻⢎⡗⡕⡅⢸⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⠄⢜⢾⣾⣿⣿⣟⣗⢯⡪⡳⡀⢸⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⠄⢸⢽⣿⣷⣿⣻⡮⡧⡳⡱⡁⢸⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⡄⢨⣻⣽⣿⣟⣿⣞⣗⡽⡸⡐⢸⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⡇⢀⢗⣿⣿⣿⣿⡿⣞⡵⡣⣊⢸⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⡀⡣⣗⣿⣿⣿⣿⣯⡯⡺⣼⠎⣿⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣧⠐⡵⣻⣟⣯⣿⣷⣟⣝⢞⡿⢹⣿⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⡆⢘⡺⣽⢿⣻⣿⣗⡷⣹⢩⢃⢿⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⣷⠄⠪⣯⣟⣿⢯⣿⣻⣜⢎⢆⠜⣿⣿⣿⣿⣿
+echo	⣿⣿⣿⣿⣿⡆⠄⢣⣻⣽⣿⣿⣟⣾⡮⡺⡸⠸⣿⣿⣿⣿
+echo	⣿⣿⡿⠛⠉⠁⠄⢕⡳⣽⡾⣿⢽⣯⡿⣮⢚⣅⠹⣿⣿⣿
+echo	⡿⠋⠄⠄⠄⠄⢀⠒⠝⣞⢿⡿⣿⣽⢿⡽⣧⣳⡅⠌⠻⣿
+echo	⠁⠄⠄⠄⠄⠄⠐⡐⠱⡱⣻⡻⣝⣮⣟⣿⣻⣟⣻⡺⣊⣿
+
+echo
+
+echo TROXA kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+pause
